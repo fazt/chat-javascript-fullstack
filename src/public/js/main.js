082934da3563183b666cbccb9f8aafd24c961bb5
Fabian <fabianncdev@gmail.com>
2018-04-04 12:09:28 -0500

    usernames added

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,10 +3,35 @@ $(function () {
     // socket.io client side connection
     const socket = io.connect();
 
-    // obtaining DOM elements from the HTML Interface
+    // obtaining DOM elements from the Chat Interface
     const $messageForm = $('#message-form');
     const $messageBox = $('#message');
     const $chat = $('#chat');
+
+    // obtaining DOM elements from the NicknameForm Interface
+    const $nickForm = $('#nickForm');
+    const $nickError = $('#nickError');
+    const $nickname = $('#nickname');
+
+    // obtaining the usernames container DOM
+    const $users = $('#usernames');
+
+    $nickForm.submit(e => {
+      e.preventDefault();
+      socket.emit('new user', $nickname.val(), data => {
+        if(data) {
+          $('#nickWrap').hide();
+          $('#contentWrap').show();
+        } else {
+          $nickError.html(`
+            <div class="alert alert-danger">
+              That username already Exists.
+            </div>
+          `);
+        }
+      });
+      $nickname.val('');
+    });
 
     // events
     $messageForm.submit( e => {
@@ -16,6 +41,15 @@ $(function () {
     });
 
     socket.on('new message', data => {
-      $chat.append(data + '<br/>');
+      $chat.append('<b>' +data.nick + ': </b>' + data.msg + '<br/>');
     });
+
+    socket.on('usernames', data => {
+      let html = '';
+      for(i = 0; i < data.length; i++) {
+        html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`; 
+      }
+      $users.html(html);
+    });
+
 });
--- src/sockets.js
+++ src/sockets.js
@@ -1,12 +1,37 @@
 module.exports = io => {
-  
+
+  let nicknames = [];
+
   io.sockets.on('connection', socket => {
+
+    socket.on('new user', (data, cb) => {
+      if (nicknames.indexOf(data) != -1) {
+        cb(false);
+      } else {
+        cb(true);
+        socket.nickname = data;
+        nicknames.push(socket.nickname);
+        updateNicknames();
+      }
+    });
 
     // receive a message a broadcasting
     socket.on('send message', data => {
-      io.sockets.emit('new message', data);
+      io.sockets.emit('new message', {
+        msg: data,
+        nick: socket.nickname
+      });
     });
 
+    socket.on('disconnect', data => {
+      if(!socket.nickname) return;
+      nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+      updateNicknames();
+    });
+
+    function updateNicknames() {
+      io.sockets.emit('usernames', nicknames);
+    }
   });
 
 }
