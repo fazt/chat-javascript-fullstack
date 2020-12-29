9533eb278ddad2cd7d04565824a5ba5176e0eb0a
Jonathan20021 <Jonathan20021@security.github.com>
2020-12-29 03:45:20 -0400

    agregado mongodb

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/chat', {
+mongoose.connect('mongodb+srv://Jonathan:Jonathansf2002@cluster0.cqufb.mongodb.net/ChatJS?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
