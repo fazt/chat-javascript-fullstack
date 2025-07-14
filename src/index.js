fce32b83b052efbcff2cef12cacd7f2bee5412d0
fazt <fazt@faztweb.com>
2025-07-14 14:09:55 -0500

    updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import app from "./app";
-import "./database";
+import app from "./app.js";
+import "./database.js";
 import http from "http";
-import socketio from "socket.io";
-import sockets from "./sockets";
+import { Server } from "socket.io";
+import sockets from "./sockets.js";
 
 const server = http.createServer(app);
-const io = socketio(server);
+const io = new Server(server);
 
 sockets(io);
 
--- src/sockets.js
+++ src/sockets.js
@@ -1,4 +1,4 @@
-import Chat from './models/Chat'
+import Chat from './models/Chat.js'
 
 export default io => {
 
